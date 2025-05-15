4e9f464351cd748e6214f2135da71d388a395bb8
Artenair <gattosamurai@gmail.com>
2025-05-16 00:34:48 +0200

    Adding deploy procedure to main bot.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ const bot = new BotGateway();
 bot.setup();
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-const GUILD_TOKEN = process.env.DISCORD_GUILD_ID;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_APPLICATION_ID = process.env.DISCORD_APPLICATION_ID;
+
+if(process.env.DISCORD_PRODUCTION_GUILD_ID) {
+    bot.deploy(
+        DISCORD_TOKEN,
+        DISCORD_APPLICATION_ID, 
+        process.env.DISCORD_PRODUCTION_GUILD_ID, 
+        Commands
+    );
+}
+
+if(process.env.DISCORD_DEVELOPMENT_GUILD_ID) {
+    bot.deploy(
+        DISCORD_TOKEN,
+        DISCORD_APPLICATION_ID,
+        process.env.DISCORD_DEVELOPMENT_GUILD_ID,
+        Commands
+    );
+}
+
 Commands.map((command) => bot.addCommand(command))
-bot.run(DISCORD_TOKEN, GUILD_TOKEN)
+bot.run(DISCORD_TOKEN)
